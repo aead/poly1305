384a61c8cde26c6c4c86ec889139dba8b04b98bc
Andreas Auernhammer <aead@mail.de>
2016-08-11 17:04:37 +0200

    Refactor assembly
    Fix AX and DX register in MULTIPLY makro. Save one instruction in MULTIPLY loop

--- poly1305_amd64.s
+++ poly1305_amd64.s
@@ -12,48 +12,48 @@
 	LEAQ 16(msg), msg; \
 	ADCQ $1,h2
 	
-#define POLY1305_MUL(h0, h1, h2, r0, r1, t0, t1, R11, R12, R13, R14) \
-	MOVQ r0, t0; \
+#define POLY1305_MUL(h0, h1, h2, r0, r1, t0, t1, t2, t3) \
+	MOVQ r0, AX; \
 	MULQ h0; \
-	MOVQ t0, R11; \
-	MOVQ t1, R12; \
-	MOVQ r0, t0; \
+	MOVQ AX, t0; \
+	MOVQ DX, t1; \
+	MOVQ r0, AX; \
 	MULQ h1; \
-	ADDQ t0, R12; \
-	ADCQ $0, t1; \
-	MOVQ r0, R13; \
-	IMULQ h2, R13; \
-	ADDQ t1, R13; \
+	ADDQ AX, t1; \
+	ADCQ $0, DX; \
+	MOVQ r0, t2; \
+	IMULQ h2, t2; \
+	ADDQ DX, t2; \
     				\
-	MOVQ r1, t0; \
+	MOVQ r1, AX; \
 	MULQ h0; \
-	ADDQ t0, R12; \
-	ADCQ $0, t1; \
-	MOVQ t1, h0; \
-	MOVQ r1, t0; \
+	ADDQ AX, t1; \
+	ADCQ $0, DX; \
+	MOVQ DX, h0; \
+	MOVQ r1, AX; \
 	MULQ h1; \
-	ADDQ h0, R13; \
-	ADCQ $0, t1; \
-	ADDQ t0, R13; \
-	ADCQ $0, t1; \
-	MOVQ r1, R14; \
-	IMULQ h2, R14; \
-	ADDQ t1, R14; \
+	ADDQ h0, t2; \
+	ADCQ $0, DX; \
+	ADDQ AX, t2; \
+	ADCQ $0, DX; \
+	MOVQ r1, t3; \
+	IMULQ h2, t3; \
+	ADDQ DX, t3; \
 					\
-	MOVQ R11, h0; \
-	MOVQ R12, h1; \
-	MOVQ R13, h2; \
+	MOVQ t0, h0; \
+	MOVQ t1, h1; \
+	MOVQ t2, h2; \
 	ANDQ $3, h2; \
-	MOVQ R13, R11; \
-	MOVQ R14, R12; \
-	ANDQ $0XFFFFFFFFFFFFFFFC, R11; \
-	SHRQ $2, R14, R13; \
-	SHRQ $2, R14; \
-	ADDQ R11, h0; \
-	ADCQ R12, h1; \
+	MOVQ t2, t0; \
+	MOVQ t3, t1; \
+	ANDQ $0XFFFFFFFFFFFFFFFC, t0; \
+	ADDQ t0, h0; \
+	ADCQ t3, h1; \
 	ADCQ $0, h2; \
-	ADDQ R13, h0; \
-	ADCQ R14, h1; \
+	SHRQ $2, t3, t2; \
+	SHRQ $2, t3; \
+	ADDQ t2, h0; \
+	ADCQ t3, h1; \
 	ADCQ $0, h2
 
 DATA poly1305Mask<>+0x00(SB)/8, $0x0FFFFFFC0FFFFFFF
@@ -100,51 +100,50 @@ TEXT ·finalize(SB),$0-16
 TEXT ·core(SB),$0-32
 	MOVQ state+0(FP), DI
 	MOVQ msg_base+8(FP), SI
-	MOVQ msg_len+16(FP), R10
+	MOVQ msg_len+16(FP), R15
 	
-	MOVQ 0(DI), BX		// h0
-	MOVQ 8(DI), CX		// h1
-	MOVQ 16(DI), R9		// h2
-	MOVQ 24(DI), R15	// r0
-	MOVQ 32(DI), R8 	// h1
+	MOVQ 0(DI), R8		// h0
+	MOVQ 8(DI), R9		// h1
+	MOVQ 16(DI), R10	// h2
+	MOVQ 24(DI), R11	// r0
+	MOVQ 32(DI), R12 	// h1
 	
-	CMPQ	R10,$16
+	CMPQ	R15,$16
 	JB BYTES_BETWEEN_0_AND_15
 	
 LOOP:
-	POLY1305_ADD(SI, BX, CX, R9)
+	POLY1305_ADD(SI, R8, R9, R10)
 MULTIPLY:
-	POLY1305_MUL(BX, CX, R9, R15, R8, AX, DX, R11, R12, R13, R14)
-	SUBQ	$16,R10
-	CMPQ	R10,$16
+	POLY1305_MUL(R8, R9, R10, R11, R12, BX, CX, R13, R14)
+	SUBQ $16, R15
+	CMPQ R15, $16
 	JAE	LOOP
 	
 BYTES_BETWEEN_0_AND_15:
-	TESTQ R10,R10
+	TESTQ R15, R15
 	JZ	DONE
-	MOVQ $1, R11
-	XORQ R12, R12
+	MOVQ $1, BX
+	XORQ CX, CX
 	XORQ R13, R13
-	ADDQ R10, SI
+	ADDQ R15, SI
 	
 FLUSH_BUFFER:
-	SHLQ $8, R11, R12
-	SHLQ $8, R11
-	XORQ R13, R13
+	SHLQ $8, BX, CX
+	SHLQ $8, BX
 	MOVB -1(SI), R13
-	XORQ R13, R11
+	XORQ R13, BX
 	DECQ SI
-	DECQ R10
+	DECQ R15
 	JNZ	FLUSH_BUFFER
 	
-	ADDQ R11, BX
-	ADCQ R12, CX
-	ADCQ $0, R9
-	MOVQ $16, R10
+	ADDQ BX, R8
+	ADCQ CX, R9
+	ADCQ $0, R10
+	MOVQ $16, R15
 	JMP	MULTIPLY	
 	
 DONE:
-	MOVQ BX, 0(DI)
-	MOVQ CX, 8(DI)
-	MOVQ R9, 16(DI)
+	MOVQ R8, 0(DI)
+	MOVQ R9, 8(DI)
+	MOVQ R10, 16(DI)
 	RET
